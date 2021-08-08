c0541b82442a38508b06aad36e0adbfa60b07f46
Muhammad Ariful Furqon <44829627+Ariful49@users.noreply.github.com>
2021-08-08 19:44:19 +0700

    refactor section 1

--- src/fga/jp/section101/Lesson101.java
+++ src/fga/jp/section101/Lesson101.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fga.jp.section101;
+package fga.jp.section1;
 import java.util.Scanner;
 /**
  *
